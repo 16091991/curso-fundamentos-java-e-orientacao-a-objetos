58172bef6d448dd7ef4a993d8d7b69c2c343ba70
Bruno Camilo da Silva <brunoc.silva.ext@ultragaz.com.br>
2022-10-27 20:18:34 -0300

    2.10. Exercício: variáveis e operadores aritméticos

---
+++ Aulas/2.7-Trabalhando-com-variaveis/Variavel1.java
@@ -0,0 +1,8 @@
+public class Variavel1{
+public static void main (String [] args){
+	int quantidade =  10;
+	System.out.println(quantidade);
+	quantidade = 15;
+	System.out.println(quantidade);
+}
+}
---
+++ Aulas/2.8-Nomeando-variaveis/Variavel1.java
@@ -0,0 +1,18 @@
+public class Variavel1{
+	public static void main (String [] args){
+	int quantidade;
+	int quantidade_alunos;
+  int QUANTIDADE;
+	int QuantidadeAlunos;
+  int $quantidade;
+  int _quantidade;
+  int quantidade_alunos_nota_10;	
+	
+// declarações de variáveis que vão apresentar erro.
+
+	int 2alunos;
+  int quantidade alunos;
+  int new;
+
+	}
+}
