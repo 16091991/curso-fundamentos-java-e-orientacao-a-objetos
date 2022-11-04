64b772c67b6437305cab801ea3b65845d5b2c697
Bruno Camilo da Silva <brunoc.silva.ext@ultragaz.com.br>
2022-11-03 21:22:01 -0300

    Aulas/2.17-Recebendo-entrada/

---
+++ Aulas/2.13-Conversao-de-tipos-primitivos/ConversaoTiposPrimitivos.java
@@ -0,0 +1,7 @@
+public class ConversaoTiposPrimitivos{
+	public static void main (String[] args){
+		double a = 20.5D;
+		float b = (float)a;
+    System.out.println(a);
+  }
+}
---
+++ Aulas/2.16-Trabalhando-com-strings/TrabalhandoComStrings.java
@@ -0,0 +1,9 @@
+public class TrabalhandoComStrings{
+	public static void main (String[]args){
+		int x = 10;
+		int y = 5;
+		int z = x + y;
+
+		System.out.println("Resultado: "+ z + y);
+		}
+}
